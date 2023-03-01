674a4ecbe58778fdb304e2f5a18d0ae1899c7782
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-03-01 13:45:25 +0300

    Create Dontrun.py

---
+++ Dontrun.py
@@ -0,0 +1,9 @@
+import platform
+import os
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("privet").ninex()
+else:
+	exit(f' Unknow device machine {arc}')
